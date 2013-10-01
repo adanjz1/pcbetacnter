7e0c9bb77a0dea7dfa5c8d1f6eb9cc174528e873
adan <adan@aceroso.(none)>
2013-10-01 00:54:57 -0300

    some changes

--- ci/application/views/widgets/header.php
+++ ci/application/views/widgets/header.php
@@ -17,6 +17,16 @@
         
     </head>
     <body>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-43198953-1', 'pccounter.net');
+            ga('send', 'pageview');
+
+          </script>
         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
         <script type="text/javascript" src="{siteUrlMedia}media/js/main.js"></script>
         <script type="text/javascript" src="{siteUrlMedia}media/js/code.js"></script>
