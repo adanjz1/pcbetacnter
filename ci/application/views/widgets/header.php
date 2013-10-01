44c1217f2360bc4ed79f9f6b86f3c092f3f27013
adan <adan@aceroso.(none)>
2013-10-01 02:43:06 -0300

    coupons fix

--- ci/application/views/widgets/header.php
+++ ci/application/views/widgets/header.php
@@ -17,21 +17,16 @@
         
     </head>
     <body>
-        <script type="text/javascript">
+       <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            var _gaq = _gaq || [];
+  ga('create', 'UA-43197448-2', 'pccounter.net');
+  ga('send', 'pageview');
 
-            _gaq.push(['_setAccount', 'UA-43198953-1']);
-
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-
-          </script>
+</script>
         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
         <script type="text/javascript" src="{siteUrlMedia}media/js/main.js"></script>
         <script type="text/javascript" src="{siteUrlMedia}media/js/code.js"></script>
