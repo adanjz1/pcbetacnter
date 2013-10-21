41f09b90410f090952d159a05cc27cf971390818
adan <adan@adan-laptop.(none)>
2013-10-21 00:48:37 -0300

    app

--- application/controllers/deals.php
+++ application/controllers/deals.php
@@ -1,4 +1,4 @@
-rev<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Deals extends CI_Controller {
 
